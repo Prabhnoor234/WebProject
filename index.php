2b8603a3cf05670438910dcbd4ef797ac88f86b2
Prabhnoor234 <118402853+Prabhnoor234@users.noreply.github.com>
2022-11-16 15:21:15 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Noorz_Bake_O_Magic</title>
+	<link rel="stylesheet" type="text/css" href="WebProject.css">
+</head>
+<body>
+	<div class="first">
+		<nav>
+			<h1 class="logo">Noorz Bake-O-Magic</h1>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="About_Us.php">About Us</a></li>
+				<li><a href="login.php">Login</a></li>
+				<li><a href="#">Skills</a></li>
+				<li><a href="#">Contact US</a></li>
+			</ul>
+			<a href="#" class="btn">Subscribe</a>
+		</nav>
+		<div class="content">
+			<h2>Celebrate your special day with US.</h2>
+			<h4>Deserts are the fairy tales of the kitchen-a happily ever- after to supper</h4>
+		</div>
+	</div>
+</body>
+</html>
