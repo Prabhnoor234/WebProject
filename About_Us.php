40f15818cb294c0550e25f11744a55b4044a7eb4
Prabhnoor234 <118402853+Prabhnoor234@users.noreply.github.com>
2022-11-16 16:26:04 -0800

    Add files via upload

---
+++ About_Us.php
@@ -0,0 +1,20 @@
+about<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>About Us</title>
+	<link rel="stylesheet" type="text/css" href="WebProject.css">
+</head>
+<body>
+	<section class="about">
+		<div class="main">
+			<img src="images/cake1.jpg">
+			<div class="text">
+				<h2>About <span>Me</span></h2>
+				<p>Hello, my name is Prabhnoor. I am very passionate about baking and also a newbie in this field. I started cake decorating as a hobby very recently in India.I am a self taught baker.I am so excited to be here and feel really confident that I have so many experienced blessed bakers around me with whom I can interact and clarify my doubts.</p>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
